9d0e6bf6d682ca53836d500bf836031bc57b7d8f
JornTumrongwit <jorn1013@gmail.com>
2024-03-03 20:58:01 +0700

    Feat: simple login page, no login system with backend yet

--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -1,8 +1,54 @@
 <template>
-  <v-container>
-    <div>
-      <h1>This is the login page</h1>
-    </div>
-  </v-container>
+  <v-sheet class="mx-auto" width="300">
+    <v-form ref="form">
+      <v-text-field
+        v-model="username"
+        :rules="usernameRules"
+        label="Username"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        type="password"
+        v-model="password"
+        :rules="passwordRules"
+        label="Password"
+        required
+      ></v-text-field>
+
+      <div class="d-flex flex-column">
+        <v-btn class="mt-4" color="success" block @click="submit">
+          Login
+        </v-btn>
+
+        <v-btn class="mt-4" color="error" block @click="reset"> Reset </v-btn>
+      </div>
+    </v-form>
+  </v-sheet>
 </template>
-<script setup></script>
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    username: "",
+    usernameRules: [(v) => !!v || "Username is required"],
+    password: "",
+    passwordRules: [(v) => !!v || "Password is required"],
+    select: null,
+  }),
+
+  methods: {
+    async submit() {
+      const { valid } = await this.$refs.form.validate();
+
+      if (valid) alert("Form is valid");
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    },
+  },
+};
+</script>
