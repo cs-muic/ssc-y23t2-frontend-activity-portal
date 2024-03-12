97f1106e56773376e4b98ef6dcd3b6cf86b565cb
AchiraLaovong <achiralaovong@gmail.com>
2024-03-12 17:55:29 +0700

    fix: wrong color for the button

--- frontend/src/views/UnauthorizedHomePage.vue
+++ frontend/src/views/UnauthorizedHomePage.vue
@@ -3,7 +3,7 @@
     <h2 class="title">Welcome to Activity Portal!</h2>
     <p>To get started, you can log in or sign up to continue.</p>
 
-    <v-btn class="mt-4" color="#ad1d5" block @click="login"> Log In </v-btn>
+    <v-btn class="mt-4" color="#ad1d25" block @click="login"> Log In </v-btn>
     <v-btn class="mt-4" color="#3cb371" block @click="createAccount">
       Create Account
     </v-btn>
