06b4c7ca95d66e36b1da165b833edfd820fc7020
Natthapee Sriarunluck <117160306+NatthapeeSriarunluck@users.noreply.github.com>
2024-03-25 09:43:23 +0700

    feat: Group activity page has a button that has a working edit button

--- frontend/src/views/activity/GroupActivityPage.vue
+++ frontend/src/views/activity/GroupActivityPage.vue
@@ -5,9 +5,9 @@
     </div>
     <v-divider :thickness="20" class="border-opacity-0"></v-divider>
     <v-data-table :headers="headers" :items="activities">
-      <!--      <template v-slot:[`item.action`]="{ item }">-->
-      <!--        <v-btn @click="unjoinActivity(item.id)" color="error"> Unjoin</v-btn>-->
-      <!--      </template>-->
+      <template v-slot:[`item.action`]="{ item }">
+        <v-btn @click="editActivity(item.id)" color="error">Edit</v-btn>
+      </template>
       <template v-slot:[`item.start_time`]="{ item }">
         {{ formatDate(item.start_time) }}
       </template>
@@ -50,6 +50,11 @@ export default {
     };
   },
   methods: {
+    editActivity(activityId) {
+      router.push(
+        `/group/${this.$route.params.groupID}/edit-activity/${activityId}`
+      );
+    },
     getGroupInfo() {
       axios
         .get(`/api/group-search/${this.$route.params.groupID}`)
